fdfd5079301dd66ef7a94aa6bac00162e772708f
Luis Jauregui <luis.jauregui.fl@gmail.com>
2021-11-01 13:08:31 -0600

    fixed code cliamte issue

--- GoToProject/src/containers/SignUp/index.js
+++ GoToProject/src/containers/SignUp/index.js
@@ -1,44 +1,16 @@
-import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import React from 'react';
 import {RecordHeader} from '../../components/RecordHeader';
 import {
-  CustomButton,
-  TextButton,
-  ButtonIcon,
-  ImageLabel,
-  InputLabel,
-  InputText,
-  FlexContainer,
+
   Layout,
 } from '../../components/styled';
-import {Formik} from 'formik';
-import {colors} from '../../library/constants/colors';
-import Icon from 'react-native-ionicons';
 import { CustomInput } from '../../components/customInput';
 import { ButtonCustom } from '../../components/ButtonCustom';
 
 export const SignUp = () => {
-  const [shwPassword, setShowPassword] = useState(true);
 
-  const handleSignIn = values => {
-    const {email, password, username} = values;
-    (() => {
-      setStatus('signedUp');
-    })
-      .catch(() => {
-        setStatus('false');
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
   return (
-    <Formik
-      initialValues={{
-        email: '',
-        password: '',
-      }}>
-      {({values}) => (
+   
         <Layout>
           <RecordHeader title="Sign Up" />
           <CustomInput label="Full Name" />
@@ -46,7 +18,5 @@ export const SignUp = () => {
           <CustomInput label="Password" />
           <ButtonCustom h="20%" mt="50px" text="Sign Up" />
         </Layout>
-      )}
-    </Formik>
   );
 };
