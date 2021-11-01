a05eb90006e70de59f5167e02780989426feaed0
Luis Jauregui <luis.jauregui.fl@gmail.com>
2021-11-01 13:03:43 -0600

    Sign Up view

--- GoToProject/src/components/CustomInput/index.js
+++ GoToProject/src/components/CustomInput/index.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import { FlexContainer, InputLabel, InputText } from '../styled'
+import React from 'react';
+import {FlexContainer, InputLabel, InputText} from '../styled';
 
 export const CustomInput = ({...props}) => {
-    return (
-        <FlexContainer h="100px" alin >
-            <InputLabel>{props.label}</InputLabel>
-            <InputText />
-        </FlexContainer>
-    )
-}
-
+  return (
+    <FlexContainer h="15%" alin>
+      <InputLabel>{props.label}</InputLabel>
+      <InputText />
+    </FlexContainer>
+  );
+};
