3a4fcc362d991ffb742e635d949e744b576d242f
Luis Jauregui <luis.jauregui.fl@gmail.com>
2021-11-01 18:20:07 -0600

    sign up input fields displayed using map

--- GoToProject/src/containers/SignUp/index.js
+++ GoToProject/src/containers/SignUp/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import {RecordHeader} from '../../components/RecordHeader';
-import { Layout } from '../../components/styled';
-import { CustomInput } from '../../components/CustomInput';
-import { ButtonCustom } from '../../components/ButtonCustom';
+import {Layout} from '../../components/styled';
+import {CustomInput} from '../../components/CustomInput';
+import {ButtonCustom} from '../../components/ButtonCustom';
+
+const signUpInputs = [
+  {
+    key: 'Full name'
+  },
+  {
+    key: 'Email'
+  },
+  {
+    key: 'Password'
+  },
+];
 
 export const SignUp = () => {
-
-  return (   
-        <Layout>
-          <RecordHeader title="Sign Up" />
-          <CustomInput label="Full Name" />
-          <CustomInput label="Email" />
-          <CustomInput label="Password" />
-          <ButtonCustom h="20%" mt="50px" text="Sign Up" />
-        </Layout>
+  const inputList = signUpInputs.map(inputName => (
+    <CustomInput label={inputName.key} />    
+  ));
+  return (
+    <Layout>
+      <RecordHeader title="Sign Up" />
+      {inputList}
+      <ButtonCustom h="20%" mt="50px" text="Sign Up" />
+    </Layout>
   );
 };
