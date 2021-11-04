3aac45aaeb712f2d687d2786b6e3f37df654f8a4
RaulMorales404 <r20160106@gmail.com>
2021-11-04 17:11:13 -0600

    reactions screen correction

--- GoToProject/App.js
+++ GoToProject/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Home} from './src/containers/Home';
 import {Login} from './src/containers/Login';
-import {Reactions} from './src/containers/Reactions/index';
+import { Reactions } from './src/containers/Reactions';
 import { Menu } from './src/components/menu';
 import { SignUp } from './src/containers/SignUp';
 import { WorkShedules } from './src/containers/WorkSchedules';
-import { Reactions } from './src/containers/Reactions';
+;
 const App = () => {
   return <Reactions />
 };
