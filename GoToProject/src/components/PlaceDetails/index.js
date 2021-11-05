c8cfd12441c0fc54561c29696e4b2a1e0d6e51fa
Luis Jauregui <luis.jauregui.fl@gmail.com>
2021-11-05 00:49:08 -0600

    added icons on details screen

--- GoToProject/src/components/PlaceDetails/index.js
+++ GoToProject/src/components/PlaceDetails/index.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import {FlexContainer, TitleText} from '../styled';
+import Icon from 'react-native-ionicons';
+import {colors} from '../../library/constants/colors';
 
 const rateTitles = [
   {
-    title: "Category",
+    title: 'Category',
   },
   {
-    title: "Stars",
+    title: 'Stars',
   },
   {
-    title: "Schedules",
+    title: 'Schedules',
   },
 ];
 
+const reactionIcons = [
+  {name: 'star'},
+  {name: 'eye'},
+  {name: 'heart'},
+];
 const titlesList = rateTitles.map(titles => (
-    <TitleText fonz="22px" key={titles.title}>{titles.title} </TitleText>    
-  ));
+  <TitleText fonz="22px" key={titles.title}>
+    {titles.title} </TitleText>
+));
+
+const iconList = reactionIcons.map(names => ( 
+    <Icon key={names.name} name={names.name} size={30} color={colors.white} />
+));
 export const PlaceDetails = () => {
-    
   return (
-    <FlexContainer h="400px" dir="row">
+    <FlexContainer h="400px" dir="row" alin >
       <FlexContainer w="55%">
         <TitleText fonz="30px">Piedra Acampanada</TitleText>
         {titlesList}
       </FlexContainer>
-      <FlexContainer w="45%">
-        <TitleText>Rate icons</TitleText>
+      <FlexContainer h="65%" w="45%">
+        {iconList}
       </FlexContainer>
     </FlexContainer>
   );
