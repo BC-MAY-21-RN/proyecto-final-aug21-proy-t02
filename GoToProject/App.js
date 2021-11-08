aeca8f728d045b6b0b650840973193c8e5e463f6
Luis Jáuregui <luis.jauregui.fl@gmail.com>
2021-11-07 22:44:38 -0600

    app file conflicts resolved

--- GoToProject/App.js
+++ GoToProject/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import {Home} from './src/containers/home';
-import {Login} from './src/containers/Login';
 import { Menu } from './src/components/menu';
 import { SignUp } from './src/containers/SignUp';
 import { WorkShedules } from './src/containers/WorkSchedules';
-import { DetailsScreen } from './src/containers/DetailsScreen';
 
 const App = () => {
   return (
-    <DetailsScreen />   
+    <SignUp />
+    <WorkShedules></WorkShedules>
   );
 };
 export default App;
