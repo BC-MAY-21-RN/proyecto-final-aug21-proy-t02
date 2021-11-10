bbddb8ac5527afdbe458dca0a74aed7a31e5e09f
RaulMorales404 <r20160106@gmail.com>
2021-11-10 12:57:07 -0600

    Correct the division of the components

--- GoToProject/src/components/SelectImagesActions/SelectimagePrimary.js
+++ GoToProject/src/components/SelectImagesActions/SelectimagePrimary.js
@@ -1,31 +1,21 @@
 import React from 'react';
-
-import {colors} from '../../library/constants/colors';
 import {
   ContainerImage,
   ImageButtons,
-  ButtonAction,
 } from '../../containers/SelectImagesSite /styled';
-import { OpenGaleria } from '../../components/SelectImagesActions';
-import {Image, Text} from 'react-native';
-
-import { CustomIcon } from '../CustomIcon';
+import {Image} from 'react-native';
+import {ButtonSelectImage} from './ButtonSelectImage';
 export const ShowPrimaryImage = ({state, value, def}) => {
   return (
-<ContainerImage>
+    <ContainerImage>
       <Image
         source={value ? {uri: value} : def}
         style={{borderRadius: 5, width: 330, height: 140}}
       />
       <ImageButtons>
-        <ButtonAction onPress={() => OpenGaleria(state, true)}>
-          <CustomIcon name="image" size={50} color={colors.white} />
-        </ButtonAction>
-        <ButtonAction onPress={() => OpenGaleria(state, false)}>
-          <CustomIcon name="camera" size={50} color={colors.white} />
-        </ButtonAction>
+        <ButtonSelectImage iconName="image" state={state} boolean={true} />
+        <ButtonSelectImage iconName="camera" state={state} boolean={false} />
       </ImageButtons>
     </ContainerImage>
-
-  )
+  );
 };
