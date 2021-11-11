be2ac07bdb90517865932dce7db61ba08a53bae7
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2021-11-11 13:34:17 -0400

    validation-fixes

--- GoToProject/src/library/constants/validationSchema.js
+++ GoToProject/src/library/constants/validationSchema.js
@@ -1,26 +1,4 @@
 import * as Yup from 'yup';
-export const logInSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('This field is required'),
-  password: Yup.string()
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-      'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
-    )
-    .required('This field is required'),
-});
-export const signUpSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('This field is required'),
-  email: Yup.string().email('Invalid email').required('This field is required'),
-  password: Yup.string()
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-      'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
-    )
-    .required('This field is required'),
-});
 
 export const addNewSite = Yup.object().shape({
   siteName: Yup.string().min(3).max(25).required(),
