f2ea1a2f7b0cc542adedea919e0b93c2beb47016
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2021-11-12 14:34:29 -0400

    add-new-site-fixes-codeclimate

--- GoToProject/src/containers/AddNewSite/index.js
+++ GoToProject/src/containers/AddNewSite/index.js
@@ -8,8 +8,10 @@ import {
 
 import { ButtonCustom } from '../../components/ButtonCustom';
 import { addNewSite } from '../../library/constants/validationSchema';
+import { newSite } from '../../library/constants/dataForm';
 
 export const AddNewSite = () => {
+  let inputNewSite
   return (
     <Formik 
       initialValues={{
@@ -20,32 +22,26 @@ export const AddNewSite = () => {
       validationSchema = { addNewSite }
       onSubmit={values => console.log(values)}>
       {({handleChange, handleSubmit, errors, touched, values}) => (
+        <>
         <Layout>
           <RecordHeader title="Add a new site"></RecordHeader>
-          <CustomInput 
-            label="Site name"
-            name="siteName"
-            onChangeText={handleChange('siteName')}
-            value={values.siteName}
-            hasErrors={errors.siteName}
-            hasTouched={touched.siteName}/>
-          <CustomInput 
-            label="Phone"
-            name="phone"
-            onChangeText={handleChange('phone')}
-            value={values.phone}
-            hasErrors={errors.phone}
-            hasTouched={touched.phone}/>
-          <CustomInput 
-            label="Select categories"
-            name="categories"
-            onChangeText={handleChange('categories')}
-            value={values.categories}
-            hasErrors={errors.categories}
-            hasTouched={touched.categories}/>
+            {
+              inputNewSite = newSite.map( entry => (
+                <CustomInput 
+                  label={entry.label}
+                  name={entry.name}
+                  onChangeText={handleChange(entry.onChangeText)}
+                  value={entry.value}
+                  value={values.value}
+                  hasErrors={errors[entry.hasErrors]}
+                  hasTouched={touched[entry.hasTouched]}
+                />
+              ))
+            }
           <ButtonCustom h="20%" mt="50px" text="Next" 
             onPress={handleSubmit}/>
         </Layout>
+        </>
       )}
     </Formik>
   );
---
+++ GoToProject/src/library/constants/dataForm.js
@@ -0,0 +1,26 @@
+export const newSite = [
+{
+  label:"Site name",
+  name:"siteName",
+  onChangeText:'siteName',
+  value:'siteName',
+  hasErrors:'siteName',
+  hasTouched:'siteName',
+},
+{
+  label:"Phone",
+  name:"phone",
+  onChangeText:'phone',
+  value:'phone',
+  hasErrors:'phone',
+  hasTouched:'phone',
+},
+{
+  label:"Select categories",
+  name:"categories",
+  onChangeText:'categories',
+  value:'categories',
+  hasErrors:'categories',
+  hasTouched:'categories',
+},
+];
