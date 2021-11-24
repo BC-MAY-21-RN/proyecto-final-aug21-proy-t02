f0f3140144023f5b3b31df35883503157bf9dda0
Vladimir Maidana <38408590+96vladi@users.noreply.github.com>
2021-11-24 15:33:04 -0400

    login-authentication

--- GoToProject/src/library/methods/auth.js
+++ GoToProject/src/library/methods/auth.js
@@ -1,6 +1,28 @@
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
+export const login = async (email, password, navigation, setStatus) => {
+  return await auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(({user}) => {
+      console.log('Ingresó');
+      setStatus(true);
+      setTimeout(() => {
+        setStatus(false);
+        navigation.navigate("Home");
+      }, 2000);
+    })
+    .catch(error => {
+      if (error.code === 'auth/wrong-password') {
+        console.log("wrong password")
+      }
+
+      if (error.code === 'auth/user-not-found') {
+        console.log('wrong email')
+      }
+    });
+};
+
 export const register = async (email, password, username,latitude,length ) => {
   return await auth()
     .createUserWithEmailAndPassword(email, password)
